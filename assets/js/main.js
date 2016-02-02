52549fc80a03cb8d7a2dfe97132edde94632c4df
Ivo Zinenda <izinenda@outlook.com>
2016-02-02 03:27:25 +0200

    include scripts

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+    $('.slider').slider();
+    $('.materialboxed').materialbox();
+    $('.carousel').carousel();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space

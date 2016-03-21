8bd5b8cc48da168da3eb294c900dc8249eb2d9f9
Ivo Zinenda <izinenda@outlook.com>
2016-03-21 14:17:21 +0200

    new look

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,18 @@
-(function($){
-  $(function(){
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("side-nav").style.width = "100%";
+}
 
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0%";
+}
+
+$( document ).ready(function(){
     $('.button-collapse').sideNav();
     $('.scrollspy').scrollSpy();
-    $('.slider').slider();
+    $('.slider').slider({full_width: true});
     $('.materialboxed').materialbox();
     $('.carousel').carousel();
     $('.modal-trigger').leanModal();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+});
